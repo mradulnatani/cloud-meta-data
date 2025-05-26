a34e35e6edb467e19cdc682dc111b9edd1edcd5e
Mradul <130235203+mradulnatani@users.noreply.github.com>
2025-05-26 17:38:11 +0530

    Update initial.sh while testing

--- initial.sh
+++ initial.sh
@@ -2,12 +2,15 @@ sudo apt-get update
 sudo apt install python3
 sudo apt install python3-pip
 sudo apt install python3-venv
-python3 -m venv venv
-source venv/bin/activate
-
-sudo apt update
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+https://github.com/mradulnatani/cloud-meta-data.git
+cd cloud-meta-data
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 aws configure
+
+
 
